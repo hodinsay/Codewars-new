c5ed1fe0bf8b47db9e5b7413ab0635356d5ec226
Din S Ho <dinsho@Din-S-M1.local>
2022-12-04 10:41:33 -0800

    Boolean to Word

---
+++ 8kyu/boolToWord.js
@@ -0,0 +1,22 @@
+// Convert boolean values to strings 'Yes' or 'No'
+
+// Difficulty: 8-kyu 
+
+// Language: JavaScript
+
+// Topic: Fundamentals
+
+// Description:
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// Solution:
+
+function boolToWord( bool ){
+    // Implement if...else statement!
+    if(bool === true){
+        return "Yes";
+    } else {
+        return "No"
+    }
+}
