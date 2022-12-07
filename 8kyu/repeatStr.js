6285768c0f211c8d4854a89bf11c660f0fb96e91
Din S Ho <dinsho@Din-S-M1.local>
2022-12-07 13:51:19 -0800

    String Repeat

---
+++ 8kyu/repeatStr.js
@@ -0,0 +1,23 @@
+// String Repeat 
+
+// Difficulty: 8-kyu 
+
+// Language: JavaScript
+
+// Topic: Strings, Fundamentals
+
+// Description: 
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+// Solution:
+
+function repeatStr (n, s) {
+    return s.repeat(n);
+}
+  
+// Implement repeat() method
