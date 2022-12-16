8237f661e3804c5e3412a84f42be6e7b958f09a6
Din S Ho <dinsho@Din-S-M1.domain>
2022-12-16 00:08:04 -0800

    Lost without a map

---
+++ 8kyu/lostwoMap.js
@@ -0,0 +1,25 @@
+// Beginner - Lost Without a Map 
+
+// Difficulty: 8-kyu 
+
+// Langauge: JavaScript
+
+// Topic: Fundamentals, Arrays
+
+// Description: 
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// Solution:
+
+function maps(x){
+    return x.map(number => number * 2);
+  }
+  
+  // My Thought Process: 
+  // Need to return each value in the array multiply by 2. 
+  // Implement map() method to return an array with the values. 
